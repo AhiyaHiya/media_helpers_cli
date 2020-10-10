f9713b26e29c3eb7f9a84053a20c588dd42e0783
Jaime <3794310+AhiyaHiya@users.noreply.github.com>
2020-10-10 14:51:07 -0400

    Added organize_media main file. Updated CMakeLists file for new project, project dependency manager.

---
+++ organize_media/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/**
+The idea of the program is that you have a folder full of media items, which hopefully is one type
+of media item that you want organized by date.
+
+Usage:
+organize_media --date --photos --preflight
+organize_media --date --photos --preflight
+*/
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, world!\n";
+    (void)argc;
+    (void)argv;
+}
