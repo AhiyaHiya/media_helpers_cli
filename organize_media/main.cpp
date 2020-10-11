36803c30f77845491f78af45ab89d4587e2cba47
Jaime <3794310+AhiyaHiya@users.noreply.github.com>
2020-10-10 23:28:54 -0400

    Added code to enumerate folders and gather header info

--- organize_media/main.cpp
+++ organize_media/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <boost/program_options.hpp>
+#include <vips/vips.h>
 
 /**
 The idea of the program is that you have a folder full of media items, which hopefully is one type
@@ -18,8 +19,31 @@ namespace po = boost::program_options;
 
 using namespace std;
 
+// clang-format off
+static const auto photoFileExtensions = std::vector< std::string >{
+    ".jpeg", ".tiff", ".png", ".nef",
+    ".JPEG", ".TIFF", ".PNG", ".NEF",
+    };
+// clang-format on
+
+////////////////////////////////////////////////////////////////////////////////////////
+auto GetFilesInFolder(const fs::path& workingDirectory, const vector< string >& extensions) -> vector< fs::path >;
+void ListFilesInFolder(const fs::path& workingDirectory);
+
+void* HeaderCallbackFunc(VipsImage* image, const char* field, GValue* value, void* my_data);
+
+class ScopedVips
+{
+public:
+  ScopedVips() { VIPS_INIT(nullptr); }
+
+  ~ScopedVips() {}
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv)
 {
+
   // Setup program options
   auto desc = po::options_description("Options");
   // clang-format off
@@ -57,20 +81,103 @@ int main(int argc, char** argv)
   // Let's get the files we are about to work on
   auto currentPath = fs::current_path();
   std::cout << "Current path is " << currentPath.c_str() << '\n';
-  auto photoFileExtensions = std::vector< std::string >{
-    ".jpeg", ".tiff", ".png", ".nef",
-    ".JPEG", ".TIFF", ".PNG", ".NEF",
-    };
 
-  for (const auto& file : fs::directory_iterator(currentPath))
+  // Test 1
+  ListFilesInFolder(currentPath);
+
+  // Test 2
+  if (actOnPhotos)
+  {
+    // Get the files
+    auto foundFiles = GetFilesInFolder(currentPath, photoFileExtensions);
+    std::cout << "Found files\n";
+
+    ScopedVips scopedVips;
+
+    for (auto& file : foundFiles)
+    {
+      std::cout << file << "\n";
+
+      // Get the date created from the file info
+      std::error_code failed;
+      auto            lastWriteTime = fs::last_write_time(file, failed);
+      if (failed)
+      {
+        std::cout << "Failed to get last_write_time for file" << file << "\n";
+        continue;
+      }
+
+      auto lastWriteTimeT = decltype(lastWriteTime)::clock::to_time_t(lastWriteTime);
+      std::cout << "File write time is " << std::asctime(std::localtime(&lastWriteTimeT)) << '\n';
+
+      // Get the date created from the EXIF info
+      auto vipsImage = vips_image_new_from_file(file.c_str(), NULL);
+      if (vipsImage == nullptr)
+      {
+        std::cout << "Something bad happened\n";
+      }
+      // key/value, key is field, value is field value
+      map<string, string> headerValues;
+      void* my_data = reinterpret_cast<map<string, string>*>(&headerValues);
+      auto nullIsSuccess = vips_image_map(vipsImage, HeaderCallbackFunc, my_data);
+      if(nullIsSuccess==nullptr)
+      {
+        for(const auto& entry : headerValues)
+        {
+          const auto& key = entry.first;
+          const auto& value = entry.second;
+          cout << key.c_str() << " = " << value.c_str() << "\n";
+          // printf("%s = %s\n", key.c_str(), value.c_str());
+        }
+      }
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+auto GetFilesInFolder(const fs::path& workingDirectory, const vector< string >& extensions) -> vector< fs::path >
+{
+  auto resultSet = vector< fs::path >();
+
+  for (const auto& file : fs::directory_iterator(workingDirectory))
+  {
+    for (auto& ext : extensions)
+    {
+      if (file.path().extension() == ext)
+      {
+        resultSet.push_back(file);
+        continue;
+      }
+    }
+  }
+
+  return resultSet;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+void ListFilesInFolder(const fs::path& workingDirectory)
+{
+  for (const auto& file : fs::directory_iterator(workingDirectory))
   {
     for (auto& ext : photoFileExtensions)
     {
       if (file.path().extension() == ext)
       {
-        std::cout << file.path() << std::endl;
+        std::cout << file.path() << "\n";
         continue;
       }
     }
   }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+void* HeaderCallbackFunc(VipsImage* image, const char* field, GValue* value, void* my_data)
+{
+  map<string, string>* headerValues = reinterpret_cast<map<string, string>*>(my_data);
+  char* valueAsStr = g_strdup_value_contents(value);
+  // printf("%s = %s", field, valueAsStr);
+  (*headerValues)[string(field)] = string(valueAsStr);
+  g_free(valueAsStr);
+
+  return nullptr;
 }
